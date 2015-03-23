4841007c3117f19454d66fc72db374159934d696
Michael Fürst <mail@michaelfuerst.de>
2015-03-23 10:42:24 +0100

    Version integrated for testing

---
+++ BlaChat/UpdateChecker.cs
@@ -0,0 +1,23 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.Util;
+using System.Collections.Generic;
+using Android.Views.InputMethods;
+
+namespace BlaChat
+{
+	class UpdateChecker
+	{
+	}
+
+}
+
+
